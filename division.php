8d6a0b3c3b466aedcc9c83095348fb3a84eb91c0
Ch Ahsan <Chahsan137@gmail.com>
2019-07-30 09:37:26 -0900

    Create division.php

---
+++ division.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1>This will divide two numbers</h1>
+<?=
+	$_GET[first]." / ".$_GET['last']. " = " .   $_GET['first'] / $_GET['last'] 
+?>
+  </br>
+<a href="index.php">Reset</a>
+</body>
+</html>
