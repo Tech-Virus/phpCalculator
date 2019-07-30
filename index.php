4ad6823591f8bebeb1cfcfd4c5c8a964c6e220d0
Ch Ahsan <Chahsan137@gmail.com>
2019-07-30 09:36:58 -0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<body style="background-color:#17202A;">
+
+<div style="border:solid 2px; background-color:#CF5506">
+    <center><h3>Addition</h3><center>
+<form method="get" action="addition.php">
+  First Number&nbsp;&nbsp;&nbsp;&nbsp	: &nbsp;&nbsp;&nbsp;&nbsp<input type="number" name="first"><br><br>
+  Second Number	: &nbsp;&nbsp;&nbsp;&nbsp<input type="number" name="second"><br></br>
+  <center><button style="height:30px;width:320px;" type="submit">Add</button></center></br> 
+</form>
+ </div>   
+    </br>
+    <marquee style="color:red">Follow me at www.github.com/Tech-Virus</marquee>
+   
+   
+   
+   <!--divion panel-->
+    
+  <div style="border:solid 2px; background-color:#054E5C">  
+    <center><h3>Division</h3><center>
+<form method="get" action="division.php">
+  First Number&nbsp;&nbsp;&nbsp;&nbsp	: &nbsp;&nbsp;&nbsp;&nbsp<input type="number" name="first"><br><br>
+  Second Number	: &nbsp;&nbsp;&nbsp;&nbsp<input type="number" name="last"><br></br>
+  <center><button style="height:30px;width:320px;" type="submit">Divide</button></center></br> 
+</form>
+ </div>  
+    </br>
+<button><a href="index.php" style="text-decoration:none; font-size:20px; padding:0 5px 0 5px;">Reset</a></button>
+</body>
+</html>
